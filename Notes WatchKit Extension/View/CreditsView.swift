5d1a1cf6a4219295b4d5e2e4290229c338610b93
Ronald de Lange <ronalddelange@MacBook-Pro-van-Ronald.local>
2024-05-21 22:49:12 +0200

    build8

--- Notes WatchKit Extension/View/CreditsView.swift
+++ Notes WatchKit Extension/View/CreditsView.swift
@@ -32,7 +32,7 @@ struct CreditsView: View {
         .foregroundColor(.secondary)
         .fontWeight(.light)
         
-        Text("v1.0b6")
+        Text("v1.0b8")
           .font(.footnote)
           .foregroundColor(.secondary)
           .fontWeight(.light)
